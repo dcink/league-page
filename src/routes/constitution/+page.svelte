a4a6bb4ef8d003620b11cadddce07c8be26c84a2
dcink <113326784+dcink@users.noreply.github.com>
2024-08-06 19:09:14 -0500

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -1,15 +1,103 @@
 <script>
-	import LinearProgress from '@smui/linear-progress';
-	import { Rivalry } from '$lib/components'
-	import { waitForAll } from '$lib/utils/helper';
-
-	export let data;
-	const {
-        leagueTeamManagerData,
-        playersData,
-        transactionsData,
-        recordsData,
-        playerOne,
-        playerTwo,
-    } = data;
+League Type	Keeper - Owners can designate 2 players to keep on their roster for next season. Owners may only keep 1 QB, 1 RB, 1 WR, 1 TE, or 1 DEF each season (Cannot keep 2 players within the same position).
+League Type (cont'd)	Players drafted in round 1 and 2 cannot be kept. Each keeper will be replaced +2 rounds of where previously drafted. (Exp. player drafted in 3rd round of 2022 will be kept for 1st round 2023). Keepers picked up off of waivers that were not drafted the previous year will be substituted for a 10th round pick.
+Waiver Order	FAAB Bidding
+After Games Waivers Clear (Day)	After Tuesday
+Time Players are on Waivers After Drop	No Time, Add Immediately
+Custom Daily Waivers	Monday - Waivers (Processing Time 2am CST) Tuesday - Waivers (Processing Time 2am CST)
+Lineup-Starting & Bench Spots	QB, QB, RB, WR, FLEX, FLEX, TE, DEF. 8 Bench Spots.
+Trade Deadline	Week 12
+Playoffs Start Week	Week 14
+Playoff Teams	8
+Playoff Rounds Per Week	One Week Per Round
+Playoff Seeding Rules	Seeding
+Playoffs Lower Bracket Type	None
+Teams	12
+Injured Reserve Slots	1
+Allow Players on IR with COVID19	Yes
+Allow Out Players On IR	No
+Allow Suspended Players on IR	No
+Allow NA Players on IR	No
+Allow DNR Players on IR	No
+Allow Doubtful Players on IR	No
+Allow Moves Pre-Draft	No
+Prevent Bench Players from Being Dropped After Game Starts	Yes
+Lock All FA and Waiver Moves	No
+Override League Invite Capacity	No
+Time To View {Pending Trades	None
+	Draft Settings
+Draft Type	Snake
+Available Players to Draft	All Players
+Time Per Pick	2 Minutes
+	Scoring Settings
+	Passing
+Yards	25 yards = 1 point (.04 points per yard)
+Touchdown	4 points
+2-Pt Conversion	2 points
+Interception	-2 points per interception
+	Rushing
+Yards	10 yards = 1 point (.10 points per yard)
+Touchdowns	6 points
+2-Pt Conversion	2
+	Receiving
+PPR (Active/Inactive and PPR)	.05 per reception
+Yards	10 yards = 1 point (.10 points per yard)
+Touchdowns	6 points
+2-Pt Conversion	2
+	Kicking
+0-19	NA
+20-29	NA
+30-39	NA
+40-49	NA
+50+	NA
+PAT Made	NA
+FG Missed	NA
+PAT Missed	NA
+	Team Defense
+Touchdown	6 points
+Points Allowed 0	10 points
+Points Allowed 1-6	7 points
+Points Allowed 7-13	5 points
+Points Allowed 14-20	1 point
+Points Allowed 21-27	0 points
+Points Allowed 28-34	-1 point
+Points Allowed 35+	-3 points
+Less than 100 Yards Allowed	3 points
+100-199 Yards Allowed	2 points
+200-299 Yards Allowed	1 point
+300-399 Yards Allowed	0 points
+400-499 Yards Allowed	-1 point
+500-549 Yards Allowed	-2 points
+550+ Yards Allowed	-3 points
+Sacks	1 point
+Interceptions	2 points
+Fumble Recoverys	2 points
+Safety	2 points
+Blocked Kick	3 points
+	Special Teams
+Touchdown	6 points
+Fumble Recoverys	2 points
+	Misc
+Fumble Lost	-1 point
+Fumble Recovery Touchdown	6 points
+	Favorable 4 Mid-Season Tournament (F4T)
+When	Weeks 7 & 8
+Who	4 Total Teams - Top 3 teams by record will be seeds 1-3 (most total points will be tie breaker), Top point scoring team outside first 3 will be seed 4.
+How it works	F4T teams will play 2 games weeks 7 & 8; their regular season matchup as well as their F4T matchup. The F4T format will be bracket style, held outiside the Sleeper App by the commissioner.
+Payout	$100 winner take all.
+	Betting
+Yearly Fee	$100
+Payouts	Winner - $600, Runner-Up - $200, Third Place - $100. $20 per week to each "weekly" winner ($200 total, weeks 1-6 & 9-12). $100 to the Favorable 4 Mid-Season Tournament.
+	Weekly Winners
+Week 1	Leadfoot Larry - Player in your starting lineup with the most points.
+Week 2	THROW IT IN REVERSE TERRY! - Player in your starting lineup with the largest decrease in points from week 1.
+Week 3	Sizzlin' & Piss Missilin' - QB with the most passing yards.
+Week 4	The Eiffel Tower - Highest QB/RB/WR point combination.
+Week 5	Rookie Lives Matter - Rookie with the most points.
+Week 6	Horseshoes And Hand Grenades - Lose by the smallest margin.
+Week 7	Show me your TD's - Most total team touchdowns scored.
+Week 8	National Tight End's Day - Highest scoring TE
+Week 9	Running Wild - Running back with the most points.
+Week 10	The Grim Reaper - Team with the most wins after 10 weeks (points will be tiebreaker).
+**Wildcard Week** (Weeks 1-10)	D's Get Degrees Too - Defense with the FIRST safety of the season.
 </script>
