ee1e8336a95bcfaea07da39fc00e6d6ca3524fdb
dcink <113326784+dcink@users.noreply.github.com>
2024-08-06 19:02:02 -0500

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -1,1 +1,15 @@
+<script>
+	import LinearProgress from '@smui/linear-progress';
+	import { Rivalry } from '$lib/components'
+	import { waitForAll } from '$lib/utils/helper';
 
+	export let data;
+	const {
+        leagueTeamManagerData,
+        playersData,
+        transactionsData,
+        recordsData,
+        playerOne,
+        playerTwo,
+    } = data;
+</script>
