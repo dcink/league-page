541fc210275a9b7befae6aea317b3ef6ba6da334
dcink <113326784+dcink@users.noreply.github.com>
2026-07-29 23:09:43 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -2,7 +2,7 @@
 export const leagueID = "1250996912174141440"; // your league ID
 export const leagueName = "The Funner League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
-export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
+export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
