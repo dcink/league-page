29a8e6ff72b2731d15ead9f0fe6954b8e67179ad
dcink <113326784+dcink@users.noreply.github.com>
2026-07-29 23:20:26 -0500

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -2,4 +2,4 @@
                 icon: 'sports_football',
                 label: 'Go to Constitution',
                 dest: `https://docs.google.com/spreadsheets/d/19AIiCvl4ZO2eR0-jsqm3-xhR2oW8CAEKsyMQpG8yNKE/edit?gid=0#gid=0`,
-            },
+            }
