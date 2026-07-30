a3c6f38af8cf85fc14c1540a504a38532b6125ea
dcink <113326784+dcink@users.noreply.github.com>
2026-07-29 23:28:55 -0500

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -1,1 +1,1 @@
-[LINK](https://docs.google.com/spreadsheets/d/19AIiCvl4ZO2eR0-jsqm3-xhR2oW8CAEKsyMQpG8yNKE/edit?gid=0#gid=0)
+[LINK](https://docs.google.com/spreadsheets/d/19AIiCvl4ZO2eR0-jsqm3-xhR2oW8CAEKsyMQpG8yNKE/edit?usp=sharing)
