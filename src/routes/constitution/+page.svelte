208f886e5d5faef6ba2e9d99bee8da082eb77ac8
dcink <113326784+dcink@users.noreply.github.com>
2026-07-29 22:55:45 -0500

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -1,3 +1,48 @@
-<script>
-import { League Settings } from "$static/favicons"
-</script>
+<p>		General League Settings</p>
+
+League Type	- Keeper
+1. Owners can designate 2 players to keep on their roster for next season. 
+  a. Owners may only keep 1 QB, 1 RB, 1 WR, 1 TE, or 1 DEF each season (Cannot keep 2 players within the same position).
+2. Players drafted in round 1 and 2 cannot be kept. Each keeper will be replaced +2 rounds of where previously drafted. (Exp. player drafted in 3rd round of 2022 will be kept for 1st round 2023). 
+3. Keepers picked up off of waivers that were not drafted the previous year will be substituted for a 10th round pick.
+
+Waiver Order - FAAB Bidding
+1. After Games Waivers Clear (Day)	After Tuesday
+Time Players are on Waivers After Drop= No Time, Add Immediately
+
+Custom Daily Waivers	
+1. Monday - Waivers (Processing Time 2am CST) 
+2. Tuesday - Waivers (Processing Time 2am CST)
+
+Lineup - Starting & Bench Spots	
+QB
+QB
+RB
+WR
+FLEX
+FLEX
+TE
+DEF. 
+7 Bench Spots.
+
+Trade Deadline	Week 12
+Playoffs Start Week	Week 15
+Playoff Teams	8
+Playoff Rounds Per Week	One Week Per Round
+Playoff Seeding Rules	Seeding
+Playoffs Lower Bracket Type	None
+Teams	12
+Injured Reserve Slots	1
+Allow Players on IR with COVID19	Yes
+Allow Out Players On IR	No
+Allow Suspended Players on IR	No
+Allow NA Players on IR	No
+Allow DNR Players on IR	No
+Allow Doubtful Players on IR	No
+Allow Moves Pre-Draft	No
+Prevent Bench Players from Being Dropped After Game Starts	Yes
+Lock All FA and Waiver Moves	No
+Override League Invite Capacity	No
+Time To View {Pending Trades	None
+
+</p>
